5d057964966e0a0f34cf33aad8f4ce723f806067
afmnobre <lucas_guitar1987@hotmail.com>
2022-08-30 03:57:05 -0300

    Insert RocketSeat project.

---
+++ RocketSeat/index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Serif:opsz,wght@8..144,700&display=swap" rel="stylesheet">
+        <link type="text/css" href="app/css/style.css" rel="stylesheet">
+        <title>Restaurante Menu</title>
+    </head>
+    <body>
+        <header>
+            <h1>Rocket<span>Coffee</span></h1>
+            <p>Aberto todos os dias. 8h-21h</p>
+        </header>
+        <section>
+            <h2>Brunch</h2>
+            <ul>
+                <li>
+                    <div class="details">
+                        <h3>Waffle Doce</h3>
+                        <p class="description">O Waffe Doce é feito com o melhor dos ingredientes e feito com carinho para servir sempre.</p>
+                    </div>
+                    <strong class="price">
+                        R$37.70
+                    </strong>
+                </li>
+                <li>
+                    <div class="details">
+                        <h3>Maçã Doce</h3>
+                        <p class="description">A Maçã Doce é feita com as melhores maçãs colhidas na primvavera e feito com carinho para servir sempre.</p>
+                    </div>
+                    <strong class="price">
+                        R$17.70
+                    </strong>
+                </li>                
+            </ul>
+        </section>
+        <section>
+            <h2>Bebidas</h2>
+            <ul>
+                <li>
+                    <div class="details">
+                        <h3>Vinho</h3>
+                        <p class="description">O Waffe Doce é feito com o melhor dos ingredientes e feito com carinho para servir sempre.</p>
+                    </div>
+                    <strong class="price">
+                        R$37.70
+                    </strong>
+                </li>
+                <li>
+                    <div class="details">
+                        <h3>Água com gás</h3>
+                        <p class="description">A água da melhor fonte do nosso país, com pH bem balanceado para sua saude.</p>
+                    </div>
+                    <strong class="price">
+                        R$17.70
+                    </strong>
+                </li>                
+            </ul>
+        </section>      
+    </body>
+</html>
